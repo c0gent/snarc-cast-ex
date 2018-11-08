e4150058db779c2060c7e5014bf6b8893474cb4a
c0gent <nsan1129@gmail.com>
2018-11-08 14:01:14 -0800

    Add `Weak` conversion.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 extern crate snarc;
 
 use std::sync::Arc;
-use snarc::Snarc;
+use snarc::{Snarc, Weak as SnarcWeak};
 
 trait Material {}
 
@@ -11,8 +11,11 @@ impl Material for Concrete {}
 
 fn main() {
 	let a_c: Arc<Concrete> = Arc::new(Concrete);
-    let a_m: Arc<Material> = a_c as _;
+    let _a_m: Arc<Material> = a_c as _;
 
     let s_c: Snarc<Concrete> = Snarc::new(Concrete);
-    let s_m: Snarc<Material> = s_c as _;
+    let _s_m: Snarc<Material> = s_c.clone() as _;
+
+    let sw_c: SnarcWeak<Concrete> = Snarc::downgrade(&s_c);
+    let _sw_m: SnarcWeak<Material> = sw_c as _;
 }
